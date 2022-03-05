ba132c4eaca045cdd9f3868c0f8a3d58d8ed6afb
Jon Sagara <jon@sagarasoftware.com>
2022-03-05 07:44:38 -0800

    Top-level statements in crawler. Fixed/removed feeds in feeds.txt.

--- FeedReader.TestDataCrawler/Program.cs
+++ FeedReader.TestDataCrawler/Program.cs
@@ -1,53 +1,60 @@
 ﻿using System.Text.RegularExpressions;
+using CodeHollow.FeedReader;
 
-namespace CodeHollow.FeedReader.TestDataCrawler;
+// Get all feed URLs from the file.
+var feeds = File.ReadAllLines("feeds.txt");
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        var feeds = File.ReadAllLines("feeds.txt");
+// Ensure the output folder exists.
+var outputFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+Directory.CreateDirectory(outputFolder);
 
-        Parallel.ForEach<string>(
-            feeds,
-            x =>
-                {
-                    try
-                    {
-                        Do(x);
-                    }
-                    catch { }
-                }
-            );
-
-    }
-
-    static void Do(string url)
-    {
-        var linksTask = FeedReader.GetFeedUrlsFromUrlAsync(url);
-
-        foreach (var link in linksTask.Result)
+// Process each feed. Feeds may have more than one URL.
+await Parallel.ForEachAsync<string>(
+    feeds,
+    async (feed, token) =>
         {
             try
             {
-                string? title = link.Title;
-                if (string.IsNullOrEmpty(title))
-                {
-                    title = url.Replace("https", "").Replace("http", "").Replace("www.", "");
-
-                }
-                title = Regex.Replace(title.ToLower(), "[^a-z]*", "");
-                var curl = FeedReader.GetAbsoluteFeedUrl(url, link);
-
-                var contentTask = Helpers.DownloadAsync(curl.Url);
-
-                File.WriteAllText("d:\\feeds\\" + title + "_" + Guid.NewGuid().ToString() + ".xml", contentTask.Result);
-                Console.Write("+");
+                await DoAsync(feed, outputFolder);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(link.Title + " - " + link.Url + ": " + ex.ToString());
+                Console.Error.WriteLine($"Unhandled exception for feed '{feed}': {ex}");
             }
+        }
+    );
+
+
+/// <summary>
+/// Download the content of an individual feed.
+/// </summary>
+static async Task DoAsync(string url, string outputFolder)
+{
+    var feedLinks = await FeedReader.GetFeedUrlsFromUrlAsync(url);
+
+    foreach (var fileFeedLink in feedLinks)
+    {
+        try
+        {
+            string? title = fileFeedLink.Title;
+            if (string.IsNullOrEmpty(title))
+            {
+                title = url.Replace("https", "").Replace("http", "").Replace("www.", "");
+            }
+
+            title = Regex.Replace(title.ToLower(), "[^a-z]*", "");
+            var feedLink = FeedReader.GetAbsoluteFeedUrl(url, fileFeedLink);
+
+            var content = await Helpers.DownloadAsync(feedLink.Url);
+
+            var outputFilePath = Path.Combine(outputFolder, title + "_" + Guid.NewGuid().ToString() + ".xml");
+            File.WriteAllText(outputFilePath, content);
+            Console.Write("+");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(fileFeedLink.Title + " - " + fileFeedLink.Url + ": " + ex.ToString());
         }
     }
 }
+
