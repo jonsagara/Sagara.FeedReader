7946ffa00f1c2d3b419db726d42e16e797a9c656
Jon Sagara <jon@sagarasoftware.com>
2022-03-05 08:02:37 -0800

    Removed dead project file XML. Refactored crawler.

---
+++ FeedReader.TestDataCrawler/FeedProcessor.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeHollow.FeedReader.TestDataCrawler;
+
+public static class FeedProcessor
+{
+    private static readonly Regex _nonEnglishLetters = new Regex("[^a-z]*", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+    /// <summary>
+    /// Download the content of an individual feed.
+    /// </summary>
+    public static async Task ReadFeedAsync(string rawFeedUrl, string outputFolder)
+    {
+        var feedLinks = await FeedReader.GetFeedUrlsFromUrlAsync(rawFeedUrl);
+
+        foreach (var fileFeedLink in feedLinks)
+        {
+            try
+            {
+                string? title = fileFeedLink.Title;
+                if (string.IsNullOrEmpty(title))
+                {
+                    // No feed title. Use the URL.
+                    title = rawFeedUrl
+                        .Replace("https", "")
+                        .Replace("http", "")
+                        .Replace("www.", "");
+                }
+
+                // Ensure we have an absolute URL before trying to getch its contents.
+                var feedLink = FeedReader.GetAbsoluteFeedUrl(rawFeedUrl, fileFeedLink);
+
+                // Fetch the feed's contents.
+                var content = await Helpers.DownloadAsync(feedLink.Url);
+
+                // Keep only English characters for the file we're about to write out.
+                title = _nonEnglishLetters.Replace(title, "");
+                var outputFilePath = Path.Combine(outputFolder, title + "_" + Guid.NewGuid().ToString() + ".xml");
+                File.WriteAllText(outputFilePath, content);
+
+                // Progress indicator.
+                Console.Write("+");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{fileFeedLink.Title} - {fileFeedLink.Url}: {ex}");
+            }
+        }
+    }
+}
--- FeedReader.TestDataCrawler/Program.cs
+++ FeedReader.TestDataCrawler/Program.cs
@@ -1,60 +1,24 @@
-﻿using System.Text.RegularExpressions;
-using CodeHollow.FeedReader;
+﻿using CodeHollow.FeedReader.TestDataCrawler;
 
 // Get all feed URLs from the file.
-var feeds = File.ReadAllLines("feeds.txt");
+var rawFeedUrls = File.ReadAllLines("feeds.txt");
 
 // Ensure the output folder exists.
-var outputFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "CrawledFeeds");
 Directory.CreateDirectory(outputFolder);
 
 // Process each feed. Feeds may have more than one URL.
 await Parallel.ForEachAsync<string>(
-    feeds,
-    async (feed, token) =>
+    rawFeedUrls,
+    async (rawFeedUrl, cancellationToken) =>
         {
             try
             {
-                await DoAsync(feed, outputFolder);
+                await FeedProcessor.ReadFeedAsync(rawFeedUrl, outputFolder);
             }
             catch (Exception ex)
             {
-                Console.Error.WriteLine($"Unhandled exception for feed '{feed}': {ex}");
+                Console.Error.WriteLine($"Unhandled exception for feed '{rawFeedUrl}': {ex}");
             }
         }
     );
-
-
-/// <summary>
-/// Download the content of an individual feed.
-/// </summary>
-static async Task DoAsync(string url, string outputFolder)
-{
-    var feedLinks = await FeedReader.GetFeedUrlsFromUrlAsync(url);
-
-    foreach (var fileFeedLink in feedLinks)
-    {
-        try
-        {
-            string? title = fileFeedLink.Title;
-            if (string.IsNullOrEmpty(title))
-            {
-                title = url.Replace("https", "").Replace("http", "").Replace("www.", "");
-            }
-
-            title = Regex.Replace(title.ToLower(), "[^a-z]*", "");
-            var feedLink = FeedReader.GetAbsoluteFeedUrl(url, fileFeedLink);
-
-            var content = await Helpers.DownloadAsync(feedLink.Url);
-
-            var outputFilePath = Path.Combine(outputFolder, title + "_" + Guid.NewGuid().ToString() + ".xml");
-            File.WriteAllText(outputFilePath, content);
-            Console.Write("+");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(fileFeedLink.Title + " - " + fileFeedLink.Url + ": " + ex.ToString());
-        }
-    }
-}
-
