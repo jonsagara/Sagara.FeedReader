661fa59598a0fc7b8ab1d3d89b179d531b1dcc4e
Jon Sagara <jon@sagarasoftware.com>
2022-05-10 14:36:10 -0700

    Upgraded to .NET SDK 6.0.203. Upgraded MSFT nuget packages.

--- src/Sagara.FeedReader.Tests.Instance/FeedReaderTest.cs
+++ src/Sagara.FeedReader.Tests.Instance/FeedReaderTest.cs
@@ -218,7 +218,7 @@ public class FeedReaderTest
     public async Task TestReadTimeDoctor()
     {
         var feed = await _feedReaderSvc.ReadAsync("https://www.timedoctor.com/blog/feed/").ConfigureAwait(false);
-        Assert.Equal("Time Doctor Product Blog", feed.Title);
+        Assert.Equal("Inside Time Doctor", feed.Title);
         Assert.True(feed.Items.Count > 0);
     }
 
