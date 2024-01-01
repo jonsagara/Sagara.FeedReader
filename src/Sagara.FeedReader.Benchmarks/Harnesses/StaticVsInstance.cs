175096565168ae5cb0c02bf3dc24d07ad7217752
Jon Sagara <jon@sagarasoftware.com>
2024-01-01 14:09:15 -0800

    Removed the statid API.

--- src/Sagara.FeedReader.Benchmarks/Harnesses/StaticVsInstance.cs
+++ src/Sagara.FeedReader.Benchmarks/Harnesses/StaticVsInstance.cs
@@ -6,43 +6,45 @@ using Sagara.FeedReader.Extensions;
 
 namespace Sagara.FeedReader.Benchmarks.Harnesses;
 
-[SimpleJob(RuntimeMoniker.Net60)]
-[MemoryDiagnoser]
-public class StaticVsInstance
-{
-    private readonly FeedReaderService _feedReaderSvc;
+// 2024-01-01: Removed the static API.
 
-    public StaticVsInstance()
-    {
-        var host = new HostBuilder()
-            .ConfigureServices(ConfigureServices)
-            .Build();
+//[SimpleJob(RuntimeMoniker.Net60)]
+//[MemoryDiagnoser]
+//public class StaticVsInstance
+//{
+//    private readonly FeedReaderService _feedReaderSvc;
 
-        var serviceScope = host.Services.CreateScope();
-        _feedReaderSvc = serviceScope.ServiceProvider.GetRequiredService<FeedReaderService>();
-    }
+//    public StaticVsInstance()
+//    {
+//        var host = new HostBuilder()
+//            .ConfigureServices(ConfigureServices)
+//            .Build();
 
-    [Benchmark(Baseline = true)]
-    public async Task<Feed> StaticReadFromFileAsync()
-    {
-        return await FeedReader.ReadFromFileAsync("Feeds/Rss20MicrosoftBotFranework.xml");
-    }
+//        var serviceScope = host.Services.CreateScope();
+//        _feedReaderSvc = serviceScope.ServiceProvider.GetRequiredService<FeedReaderService>();
+//    }
 
-    [Benchmark]
-    public async Task<Feed> InstanceReadFromFileAsync()
-    {
-        // This is a static method, but it differs from the original static FeedReader API.
-        //   It parses a stream instead of a byte[].
-        return await FeedReaderService.ReadFromFileAsync("Feeds/Rss20MicrosoftBotFranework.xml");
-    }
+//    [Benchmark(Baseline = true)]
+//    public async Task<Feed> StaticReadFromFileAsync()
+//    {
+//        return await FeedReader.ReadFromFileAsync("Feeds/Rss20MicrosoftBotFranework.xml");
+//    }
+
+//    [Benchmark]
+//    public async Task<Feed> InstanceReadFromFileAsync()
+//    {
+//        // This is a static method, but it differs from the original static FeedReader API.
+//        //   It parses a stream instead of a byte[].
+//        return await FeedReaderService.ReadFromFileAsync("Feeds/Rss20MicrosoftBotFranework.xml");
+//    }
 
 
-    //
-    // Private methods
-    //
+//    //
+//    // Private methods
+//    //
 
-    private void ConfigureServices(HostBuilderContext context, IServiceCollection services)
-    {
-        services.AddFeedReaderServices();
-    }
-}
+//    private void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+//    {
+//        services.AddFeedReaderServices();
+//    }
+//}
