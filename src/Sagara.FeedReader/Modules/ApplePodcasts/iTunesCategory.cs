bdeb58dcbaf00b5e44235879a2a46ab8fb89f255
Jon Sagara <jon@sagarasoftware.com>
2024-01-15 10:53:11 -0800

    Have a dedicated iTunesSubcategory type. Support ILoggerFactory in iTunesChannel.

--- src/Sagara.FeedReader/Modules/ApplePodcasts/iTunesCategory.cs
+++ src/Sagara.FeedReader/Modules/ApplePodcasts/iTunesCategory.cs
@@ -6,12 +6,24 @@
 public class iTunesCategory
 {
     /// <summary>
-    /// Top-level category text.
+    /// The category text.
     /// </summary>
     public string? Text { get; set; }
 
     /// <summary>
     /// Optional Subcategory.
     /// </summary>
-    public iTunesCategory? Subcategory { get; set; }
+    public iTunesSubcategory? Subcategory { get; set; }
+}
+
+/// <summary>
+/// Since a subcategory has no subcategories of its own, give it its own entity without
+/// a Subcategory property.
+/// </summary>
+public class iTunesSubcategory
+{
+    /// <summary>
+    /// The subcategory text.
+    /// </summary>
+    public string? Text { get; set; }
 }
--- src/Sagara.FeedReader/Modules/ApplePodcasts/iTunesChannel.cs
+++ src/Sagara.FeedReader/Modules/ApplePodcasts/iTunesChannel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Xml.Linq;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Sagara.FeedReader.Extensions;
 
 namespace Sagara.FeedReader.Modules.ApplePodcasts;
@@ -102,12 +104,18 @@ public class iTunesChannel
     public bool Complete { get; set; }
 
 
+    private readonly ILogger<iTunesChannel> _logger;
+
+
     /// <summary>
     /// .ctor
     /// </summary>
-    public iTunesChannel(XElement channelElement)
+    public iTunesChannel(XElement channelElement, ILoggerFactory? loggerFactory = null)
     {
         ArgumentNullException.ThrowIfNull(channelElement);
+
+        loggerFactory ??= NullLoggerFactory.Instance;
+        _logger = loggerFactory.CreateLogger<iTunesChannel>();
 
         var imageElement = channelElement.GetElement(namespacePrefix: NamespacePrefix, elementName: "image");
         if (imageElement is not null)
@@ -154,7 +162,7 @@ public class iTunesChannel
     // Private methods
     //
 
-    private static iTunesCategory[] ParseCategories(XElement channelElement)
+    private iTunesCategory[] ParseCategories(XElement channelElement)
     {
         // There can be many category attributes in the channel.
         return channelElement.GetElements(namespacePrefix: NamespacePrefix, elementName: "category")
@@ -167,7 +175,7 @@ public class iTunesChannel
                 var subCatElement = catElement.GetElement(namespacePrefix: NamespacePrefix, elementName: "category");
                 var subCategoryText = subCatElement?.GetAttributeValue("text");
                 var subCategory = !string.IsNullOrWhiteSpace(subCategoryText)
-                    ? new iTunesCategory { Text = subCategoryText }
+                    ? new iTunesSubcategory { Text = subCategoryText }
                     : null;
 
                 return new iTunesCategory
@@ -179,7 +187,7 @@ public class iTunesChannel
             .ToArray();
     }
 
-    private static iTunesType? ParseiTunesType(XElement channelElement)
+    private iTunesType? ParseiTunesType(XElement channelElement)
     {
         var typeElement = channelElement.GetChildElementValue(namespacePrefix: NamespacePrefix, elementName: "type");
         if (string.IsNullOrWhiteSpace(typeElement))
@@ -191,13 +199,14 @@ public class iTunesChannel
         if (!Enum.TryParse(typeElement, out iTunesType type))
         {
             // Can't parse it into an enum. Don't assume any default value.
-#warning TODO: logging?
+            _logger.LogError($"Unable to parse an {nameof(iTunesType)} enum value from the itunes:type element: {{TypeElementXml}}", typeElement.ToString());
             return null;
         }
 
         if (!Enum.IsDefined(type))
         {
             // Not a valid enum value. Don't assume any default value.
+            _logger.LogError($"Parsed {nameof(iTunesType)}, but the value '{{TypeValue}}' is not defined. itunes:type element: {{TypeElementXml}}", type, typeElement.ToString());
             return null;
         }
 
